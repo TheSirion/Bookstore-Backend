58c7d190d320255864651c390985af8a1ca6d9dd
Matheus Ribeiro <thesirion@gmail.com>
2023-09-08 20:35:16 -0300

    added .env file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+import 'dotenv/config';
 
-export const mongoDbURL =
-  "mongodb+srv://root:HeavyWeapon0909@books-store-mern.1wo7hsy.mongodb.net/books-collection?retryWrites=true&w=majority";
+export const PORT = 5555;
+export const mongoDbURL = `mongodb+srv://root:${process.env.MONGO_DB_PASSWORD}@books-store-mern.1wo7hsy.mongodb.net/books-collection?retryWrites=true&w=majority`;
